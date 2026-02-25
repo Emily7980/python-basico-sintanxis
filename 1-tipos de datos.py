02a71b4bfd149901557010e8e11a1dd1356c3361
Emily7980 <1720980@senati.pe>
2026-02-25 12:23:01 -0500

    first commit

---
+++ 1-tipos de datos.py
@@ -0,0 +1,18 @@
+#Tipos de datos en python
+#1. Tipo de dato numérico (int)
+edad = 25
+promedio = 19
+número = -50
+#2. Tipo de dato decimal (float)
+peso = 75.50
+estatura = 170.30
+temperatura = 14.70
+precio = 99.99
+#3. Tipo de dato texto (str) (' o " en python)
+letra = 'A'
+nombre = 'Emily'
+apellido = 'Torres'
+dni = '70412623'
+#4. Tipo de dato booleanos (bool)
+es_mayor_de_edad = True
+puede_volar =False
---
+++ 2-operadores.py
@@ -0,0 +1,48 @@
+#Operadores en python
+#1. Operadores aritmeticos
+suma = 5 + 5
+resta = 10 - 6
+multipliacion = 5 * 5
+division = 10 / 2
+modulo = 10 % 2
+exponente = 10 ** 2
+print (suma)
+print(resta)
+print (multipliacion)
+print(division)
+print(modulo)
+print(exponente)
+print('El resultado de la suma es:', suma)
+print('El resultado de la resta es:', resta)
+print('El resultado de la multiplicacion es:', multipliacion)
+print('El resultado de la division es:', division)
+print('El resultado del modulo es:', modulo)
+print('El resultado del exponente es:', exponente)
+
+#2. Operadores de comparacion
+print (5 == 5)  #es '==' igual a
+print (5 != 5)  #'!= se lee como diferente de
+print (10 > 5) #mayor que
+print (10 < 5) #menor que
+print (10 >= 5) #mayor igual que
+print (10 <= 5) #menor igual que
+
+#3. Operadores logicos
+v = True
+f = False
+#3.1 and (y)
+print('----------AND')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+#3.2 or (o)
+print('----------OR')
+print(v or v)
+print(v or f)
+print(f or v)
+print(f or f)
+#3.3 not (negacion)
+print('----------NOT')
+print(not v)
+print(not f)
